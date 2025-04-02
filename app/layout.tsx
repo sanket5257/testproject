5c34853c63a7a1e65e44a2491b6e89db2c529a09
John Doe <chougulesanket30@gmail.com>
2025-04-02 18:48:29 +0530

    deploy error 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
       <head>
         <meta name="robots" content="noindex" />
+
       </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
