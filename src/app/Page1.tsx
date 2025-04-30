cff720c956a099ec023dd123e7188379519bf1af
RAM <shriramgupta8141@gmail.com>
2025-04-30 13:36:47 +0530

    refactor: update Page1 component to fetch and display hot hiring data

--- src/app/Page1.tsx
+++ src/app/Page1.tsx
@@ -1,16 +1,29 @@
+"use client";
+
 import React from "react";
-
+import { useState, useEffect } from "react";
+import api from "./services/api_service";
+import { log } from "console";
 const Page1 = () => {
-  const company = [
-    "appendFile",
-    "amazon",
-    "wipro",
-    "infotech",
-    "appendFile",
-    "amazon",
-  ];
-  console.log(company);
+  interface Data {
+    name: string;
+    description: string;
+    image_url: string;
+    url: string;
+  }
+  const [data, setData] = useState<Data[]>([
+    { name: "", description: "", image_url: "", url: "" },
+  ]);
 
+  useEffect(() => {
+    async function fetchData() {
+      const response = await api.getHotHiring();
+      const result = await response.json();
+      console.log(result);
+      setData(result);
+    }
+    fetchData();
+  }, []);
   return (
     <div className=" w-full pb-10">
       <div
@@ -22,57 +35,25 @@ const Page1 = () => {
           HOT HIRING
         </h1>
 
-        <div className="bg-[url(/hirebg.jpeg)] bg-cover bg-center h-full w-[95vw] rounded-xl">
-        <div>
-          
-        </div>
-        <div className="h-full w-full">
-        <div className="h-full opacity-0 flex justify-between items-end hover:opacity-100 overflow-hidden rounded-2xl text-white font-Montserrat w-full ">
-                <div className="bg-black opacity-30 h-full w-full"></div>
-                <div className="absolute w-[90vw] px-20 py-10 flex justify-between">
-                <div >
-              <h1 className="font-bold text-3xl">COMPANY NAME</h1>
-              <p>
-                join our dynamic team as software engineer,where you will be
-                resposible for devloping
-              </p>
-            </div>
-
-            <button className="hover:bg-linear-to-bl from-[#007BFF] to-[#004C99] text-white text-lg font-bold border-2  rounded-md  h-12 w-[13vw]">
-              Apply Now
-            </button>
-
+        <div className="bg-[url({/hirebg.jpeg})] bg-cover bg-center h-full w-[95vw] rounded-xl">
+          <div></div>
+          <div className="h-full w-full">
+            <div className="h-full opacity-0 flex justify-between items-end hover:opacity-100 overflow-hidden rounded-2xl text-white font-Montserrat w-full ">
+              <div className="bg-black opacity-30 h-full w-full"></div>
+              <div className="absolute w-[90vw] px-20 py-10 flex justify-between">
+                <div>
+                  <h1 className="font-bold text-3xl">{data[0].name}</h1>
+                  <p>{data[0].description}</p>
                 </div>
-          </div>
 
-        </div>
+                <button className="hover:bg-linear-to-bl from-[#007BFF] to-[#004C99] text-white text-lg font-bold border-2  rounded-md  h-12 w-[13vw]">
+                  Apply Now
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      {/* <div
-        id="Topcompanies"
-        className=" w-full pt-14 md:pt-10 flex flex-col justify-center items-center"
-      >
-        <div className="h-full w-full pt-12 overflow-x-hidden bg-blue-100">
-          <h1 className=" text-center md:text-3xl font-Montserrat font-bold text-blue-600">
-            Top Commines Hiring Now
-          </h1>
-          <div className="flex justify-center">
-            {company.map((companyItem, index) => {
-              return (
-                <Image
-                width={0}
-                    height={0}
-                  key={index}
-                  className="md:size-50 size-16 "
-                  src="./apple.png"
-                  alt="company logo"
-                />
-              );
-            })}
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
--- src/app/services/api_service.ts
+++ src/app/services/api_service.ts
@@ -2,6 +2,16 @@ class ApiService {
   readonly base_url: string = "http://192.168.1.222:4000";
 
   // get methods
+  async getHotHiring() {
+    const response = await fetch(`${this.base_url}/hot_hiring`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+    return response;
+  }
 
   //post methods
   // async login(id: string, pw: string) {
